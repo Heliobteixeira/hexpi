860beeef3ffd7b92a12d7b86086a64cdc4402772
Heliobteixeira <Helio.b.teixeira@gmail.com>
2013-12-09 12:48:11 +0000

    Update server.py
    Corrected servo min, max and center values

--- server.py
+++ server.py
@@ -4,12 +4,12 @@ import hexmodel
 
 # Cat laser toy configuration
 SERVO_I2C_ADDRESS 	= 0x40		# I2C address of the PCA9685-based servo controller
-SERVO_XAXIS_CHANNEL = 0 		# Channel for the x axis rotation which controls laser up/down
-SERVO_YAXIS_CHANNEL = 1			# Channel for the y axis rotation which controls laser left/right
+SERVO_XAXIS_CHANNEL = 1 		# Channel for the x axis rotation which controls laser up/down
+SERVO_YAXIS_CHANNEL = 2			# Channel for the y axis rotation which controls laser left/right
 SERVO_PWM_FREQ 		= 50 		# PWM frequency for the servos in HZ (should be 50)
-SERVO_MIN 			= 150		# Minimum rotation value for the servo, should be -90 degrees of rotation.
-SERVO_MAX 			= 650		# Maximum rotation value for the servo, should be 90 degrees of rotation.
-SERVO_CENTER		= 400		# Center value for the servo, should be 0 degrees of rotation.
+SERVO_MIN 			= 90		# Minimum rotation value for the servo, should be -90 degrees of rotation.
+SERVO_MAX 			= 500		# Maximum rotation value for the servo, should be 90 degrees of rotation.
+SERVO_CENTER		= 280		# Center value for the servo, should be 0 degrees of rotation.
 
 
 import servos
